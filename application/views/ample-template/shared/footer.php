4d54303f54cb2c3decc2a5f1b3a5b6ac9a69b97b
rakharrs <rodyox.yoann@gmail.com>
2023-04-04 14:29:06 +0300

    initial commit

---
+++ application/views/ample-template/shared/footer.php
@@ -0,0 +1,21 @@
+</div>
+</div>
+</div>
+</div>
+<script src="<?=base_url('/assets/sidebar/sidebars.js')?>"></script>
+<!-- /#wrapper -->
+<!-- jQuery -->
+<script src="<?=base_url()?>/assets/plugins/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="<?=base_url()?>/assets/bootstrap/js/bootstrap.min.js"></script>
+<!-- Menu Plugin JavaScript -->
+<script src="<?=base_url()?>/assets/plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js"></script>
+<!--slimscroll JavaScript -->
+<script src="<?=base_url()?>/assets/js/jquery.slimscroll.js"></script>
+<!--Wave Effects -->
+<script src="<?=base_url()?>/assets/js/waves.js"></script>
+<!-- Custom Theme JavaScript -->
+<script src="<?=base_url()?>/assets/js/custom.min.js"></script>
+</body>
+
+</html>
